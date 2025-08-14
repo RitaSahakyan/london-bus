4c2bbad86bdfcf0f1ca7b6331c7d4a5d520efdb7
RitaSahakyan <rita.2002.sahakyan@gmail.com>
2025-08-14 22:15:32 +0400

    Scroll Down is added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,36 +1,44 @@
-import React from 'react'
-import '../styles/Header.scss'
-import logo from '../assets/Header/logo.svg'
-import langArm from '../assets/Header/Arm.svg'
-import langRus from '../assets/Header/Rus.svg'
-import langEng from '../assets/Header/Us.svg'
-
+import React from "react";
+import "../styles/Header.scss";
+import logo from "../assets/Header/logo.svg";
+import langArm from "../assets/Header/Arm.svg";
+import langRus from "../assets/Header/Rus.svg";
+import langEng from "../assets/Header/Us.svg";
 
 const Header = () => {
-    return (
-        <header className='header'>
-            <div className='header__title'>
-                <img src={logo} alt="Logo" className='header__logo'/>
-                <div className='header__text'>
-                    <span className='header__title'>LONDON</span>
-                    <span className='header__subtitle'>BUS</span>
-                </div>
-            </div>
-            <nav className='header__nav'>
-                <a href='#home'>Գլխավոր</a>
-                <a href='#about'>Մեր մասին</a>
-                <a href='#groups'>Խմբեր</a>
-                <a href='#parents'>Ծնողները մեր մասին</a>
-                <a href='#ourday'>Մեր առօրյան</a>
-                <a href='#contacts'>Կոնտակտներ</a>
-            </nav>
-            <div className='header__lang'>
-                <img src={langArm} alt='Armenian' />
-                <img src={langRus} alt='Russian' />
-                <img src={langEng} alt='English' />
-            </div>
-        </header>
-    )
+  const scrollDown = (id) => {
+    const section = document.getElementById(id);
+    if (section) {
+      section.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+
+  return (
+    <header className="header">
+      <div className="header__title">
+        <img src={logo} alt="Logo" className="header__logo" />
+        <div className="header__text">
+          <span className="header__title">LONDON</span>
+          <span className="header__subtitle">BUS</span>
+        </div>
+      </div>
+      <nav className="header__nav">
+        <ul>
+            <li onClick={() => scrollDown('home')}>Գլխավոր</li>
+            <li onClick={() => scrollDown('about')}>Մեր մասին</li>
+            <li onClick={() => scrollDown('groups')}>Խմբեր</li>
+            <li onClick={() => scrollDown('parents')}>Ծնողները մեր մասին</li>
+            <li onClick={() => scrollDown('ourday')}>Մեր առօրյան</li>
+            <li onClick={() => scrollDown('contacts')}>Կոնտակտներ</li>
+        </ul>
+      </nav>
+      <div className="header__lang">
+        <img src={langArm} alt="Armenian" />
+        <img src={langRus} alt="Russian" />
+        <img src={langEng} alt="English" />
+      </div>
+    </header>
+  );
 };
 
 export default Header;
